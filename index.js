9ccf471a706a5d43503209bcdef6b70442bcb46e
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-03-12 15:28:44 +0300

    Summary metric for measuring request durations

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ const register = new Registry();
 
 // Collect Node.js metrics
 collectDefaultMetrics({ register });
+
+// Summary metric for measuring request durations
+const requestDurationSummary = new client.Summary({
+  name: 'sample_app_summary_request_duration_seconds',
+  help: 'Summary of request durations',
+  labelNames: ['method', 'status'],
+  percentiles: [0.5, 0.75, 0.9, 0.95, 0.99]
+});
